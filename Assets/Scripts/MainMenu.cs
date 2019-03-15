273a936de0e664b647642d54299bd495313f5620
Matt Poutas <mpoutas@live.com>
2019-03-14 20:15:02 -0400

    Main Menu sans complete functionality. Play and Quit work, Settings goes to menu but other than back currently does nothing.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("This message signifies the game has been closed.");
+        Application.Quit();
+    }
+}
